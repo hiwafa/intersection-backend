08e63faea44cd4f9494d93fceaeef75e565d855f
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-29 22:42:54 +0430

    database setuped

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,11 +10,11 @@ module.exports = ({ env }) => {
         connector: "bookshelf",
         settings: {
           client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
+          host: "ec2-35-153-91-18.compute-1.amazonaws.com",
+          port: 5432,
+          database: "dcdjtshv3dr7c3",
+          username: "dyhvhpvquakdqv",
+          password: "91bdbd3fc11c4ee8852e8de45cf7a3dc60a2bbad4be2001c6ae3f10ba41be3b8",
           ssl: { rejectUnauthorized: false },
           options: {
             ssl: false,
