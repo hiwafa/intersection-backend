7e838c58c24b5fa1dd353ef500f0f54f03b6280e
Muhammad Wafa <iamprogrammercoder@gmail.com>
2021-08-31 17:18:35 +0430

    update and using env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,7 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
 
   return {
     defaultConnection: "default",
@@ -10,11 +10,11 @@ module.exports = ({ env }) => {
         connector: "bookshelf",
         settings: {
           client: "postgres",
-          host: "ec2-35-153-91-18.compute-1.amazonaws.com",
-          port: 5432,
-          database: "dcdjtshv3dr7c3",
-          username: "dyhvhpvquakdqv",
-          password: "91bdbd3fc11c4ee8852e8de45cf7a3dc60a2bbad4be2001c6ae3f10ba41be3b8",
+          host,
+          port,
+          database,
+          username: user,
+          password,
           ssl: { rejectUnauthorized: false },
           options: {  ssl: true },
         },
